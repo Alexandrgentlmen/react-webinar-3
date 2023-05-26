29d3ecdd1294725cee61d9cb95b80609c2923f61
Alexandr Gusletsov <chegeadaun@yandex.ru>
2023-05-26 18:04:01 +0300

    1st commit for myself

--- src/store/catalog/index.js
+++ src/store/catalog/index.js
@@ -1,27 +1,27 @@
-import {codeGenerator} from "../../utils";
+import { codeGenerator } from "../../utils";
 import StoreModule from "../module";
 
 class Catalog extends StoreModule {
 
-  constructor(store, name) {
-    super(store, name);
-    this.generateCode = codeGenerator(0)
-  }
+	constructor(store, name) {
+		super(store, name);
+		this.generateCode = codeGenerator(0)
+	}
 
-  initState() {
-    return {
-      list: []
-    }
-  }
+	initState() {
+		return {
+			list: []
+		}
+	}
 
-  async load() {
-    const response = await fetch('/api/v1/articles');
-    const json = await response.json();
-    this.setState({
-       ...this.getState(),
-       list: json.result.items
-    }, 'Загружены товары из АПИ');
-  }
+	async load() {
+		const response = await fetch('/api/v1/articles');
+		const json = await response.json();
+		this.setState({
+			...this.getState(),
+			list: json.result.items
+		}, 'Загружены товары из АПИ зугразил - проверка');
+	}
 }
 
 export default Catalog;
